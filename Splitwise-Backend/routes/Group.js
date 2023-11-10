0eae72fe737c3e0fcf6015ebe5ba79f0adf53068
Rohit Kulkarni <rohitkoolkarni22@gmail.com>
2023-11-11 00:48:08 +0530

    Updated the Readme.md file
    Added Documentation to the readme file

--- Splitwise-Backend/routes/Group.js
+++ Splitwise-Backend/routes/Group.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken");
-const logRequest = require("../middlewares/test");
+const logRequest = require("../middlewares/logger");
 const Group = require("../models/Group");
 const mongoose = require("mongoose");
 
